d5b76b093b638c5f6929fe52d729135813a6a329
Rich McRary <rgmcrary@gmail.com>
2017-08-20 19:12:23 -0400

    created keys.js template

--- keys.js
+++ keys.js
@@ -1,1 +1,11 @@
+// JavaScript
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
 
